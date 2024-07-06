d94ab49dc59486c5a822b86d419c0fe9ef00c17f
Hridesh <hrideshsrivastavaa@outlook.com>
2024-07-07 04:50:51 +0530

    fixed

--- otpGen.js
+++ otpGen.js
@@ -12,3 +12,20 @@ document.getElementById("clicki").addEventListener('click',()=>{
         document.getElementById("clicki").classList.remove("pseudo");
     },100);
 })
+
+
+// const res=()=>{
+//     let length=6;
+//     let ran=Math.random()*800000;
+//     let resin=Math.floor(ran)+100000;
+//     document.getElementById("content").innerHTML=`${resin}`;
+// };
+// document.getElementById("clicki").addEventListener("click",res);
+// window.addEventListener('load',res);
+
+
+
+
+
+
+
